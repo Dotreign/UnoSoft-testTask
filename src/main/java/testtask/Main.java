2f5affbf2c5a2a196647d1518eb253459b971d64
Dotreign <i.dotreign@gmail.com>
2024-08-19 16:36:01 +0300

    fix

--- src/main/java/testtask/Main.java
+++ src/main/java/testtask/Main.java
@@ -9,6 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -17,29 +18,31 @@ public class Main {
 
   static ArrayList<HashSet<String>> groups = new ArrayList<>();
   static ArrayList<HashMap<String, Integer>> wordsPositions = new ArrayList<>();
+  static HashMap<Integer, Integer> joinedGroups = new HashMap<>();
 
   public static void main(String[] args) throws Exception {
     long startTime = Instant.now().getEpochSecond();
 
     try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
       br.lines().forEach(Main::processLine);
+      long algorithmTime = Instant.now().getEpochSecond();
+      System.out.println(algorithmTime - startTime);
     }
-
     int counter = 0;
     int moreThanOneCounter = 0;
+    groups.removeAll(Collections.singleton(null));
     groups.sort((a, b) -> a.size() < b.size() ? 1 : a.size() > b.size() ? -1 : 0);
-    try (
-        Writer writer = new FileWriter("output.txt", StandardCharsets.UTF_8)
-    ) {
-      for (HashSet<String> group :
-          groups) {
+    try (Writer writer = new FileWriter("output.txt", StandardCharsets.UTF_8)) {
+      for (HashSet<String> group : groups) {
+        if (group == null) {
+          continue;
+        }
         if (group.size() > 1) {
           moreThanOneCounter += 1;
         }
         counter += 1;
         writer.write("Group " + counter + "\n");
-        for (String line :
-            group) {
+        for (String line : group) {
           writer.write(line + "\n");
         }
       }
@@ -55,10 +58,19 @@ public class Main {
   private static void processLine(String line) {
 
     Integer groupNumber = null;
+    ArrayList<Integer> otherGroups = new ArrayList<>();
+    ArrayList<String> parts = new ArrayList<>();
 
-    List<String> parts = Arrays.stream(line.split(";"))
-        .map(part -> part.substring(1, part.length() - 1)).filter(part -> !part.contains("\""))
-        .toList();
+    for (String part : line.split(";")) {
+      if (part.isEmpty()) {
+        parts.add("");
+        continue;
+      }
+      part = part.substring(1, part.length() - 1);
+      if (!part.contains("\"")) {
+        parts.add(part);
+      }
+    }
 
     for (int i = 0; i < parts.size(); i++) {
       String part = parts.get(i);
@@ -66,18 +78,43 @@ public class Main {
       if (wordsPositions.size() <= i) {
         break;
       } else {
-        if (wordsPositions.get(i).containsKey(part)) {
-          groupNumber = wordsPositions.get(i).get(part);
-          break;
+        HashMap<String, Integer> position = wordsPositions.get(i);
+        if (position.containsKey(part)) {
+          Integer num = position.get(part);
+          while (joinedGroups.containsKey(num)) {
+            num = joinedGroups.get(num);
+          }
+          if (groupNumber == null) {
+            groupNumber = num;
+          } else {
+            otherGroups.add(num);
+          }
         }
       }
     }
 
     if (groupNumber != null) {
-      HashSet<String> group = groups.get(groupNumber - 1);
-      group.add(line);
+      if (otherGroups.isEmpty()) {
+        HashSet<String> group = groups.get(groupNumber);
+        group.add(line);
+      } else {
+        HashSet<String> group = groups.get(groupNumber);
+        for (Integer num : otherGroups) {
+          if (num != groupNumber) {
+            HashSet<String> groupToMerge = groups.get(num);
+            if (groupToMerge == null) {
+              continue;
+            }
+            group.addAll(groups.get(num));
+            groups.set(num, null);
+            joinedGroups.put(num, groupNumber);
+          }
+        }
+        group.add(line);
+      }
+
     } else {
-      groupNumber = groups.size() + 1;
+      groupNumber = groups.size();
       HashSet<String> group = new HashSet<>();
       group.add(line);
       groups.add(group);
